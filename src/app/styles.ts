1c04a3558ff5a3c154f2e46661ebad52a5e7c138
Buz Carter <buzcarter@gmail.com>
2024-07-07 13:07:36 -0700

    style logging a bit;

--- src/app/styles.ts
+++ src/app/styles.ts
@@ -1,4 +1,5 @@
 import '../assets/styles/variables.css';
+import '../assets/styles/reset.css';
 import '../assets/styles/base.css';
 import '../assets/styles/icons.css';
 import '../assets/styles/buttons.css';
