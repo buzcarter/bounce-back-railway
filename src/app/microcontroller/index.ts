f9e7c0508fd3c7fa43212599a25714d26ccb1838
Buz Carter <buzcarter@gmail.com>
2024-07-06 22:34:46 -0700

    L

--- src/app/microcontroller/index.ts
+++ src/app/microcontroller/index.ts
@@ -3,4 +3,4 @@ export * from './components/Power';
 export * from './constants/ArduinoUno';
 export * from './constants/Constants';
 // export * from './components/IOBindings';
-// export * as Serial from './components/Serial';
+export * as Serial from './components/Serial';
